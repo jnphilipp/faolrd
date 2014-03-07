b31ae7d1734b65e026783ac377d1aa7f30603e58
jnphilipp <jnphilipp@gmail.com>
2014-03-07 12:22:07 +0100

    Updated LanguageDetection.

--- src/main/java/org/faolrd/LanguageDetection.java
+++ src/main/java/org/faolrd/LanguageDetection.java
@@ -7,16 +7,19 @@ import de.uni_leipzig.asv.toolbox.jLanI.kernel.RequestException;
 import de.uni_leipzig.asv.toolbox.jLanI.kernel.Response;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import org.faolrd.io.FReader;
 import org.faolrd.net.ProxyManager;
 import org.faolrd.parser.html.sites.GoogleWebSearchHTMLParser;
 import org.faolrd.parser.html.sites.HideMyAssHTMLParser;
-import org.faolrd.results.Meta;
-import org.faolrd.results.google.GoogleMeta;
+import org.faolrd.results.Result;
 import org.faolrd.utils.Helpers;
 
 /**
@@ -29,29 +32,55 @@ public class LanguageDetection {
 	 * language
 	 */
 	private String language;
+	/**
+	 * wordlist
+	 */
+	private String wordlistFile;
 
 	/**
 	 * Retrieves the word list and queries Google.
 	 * @throws Exception 
 	 */
 	public void start() throws Exception {
-		String wordlist_file = Helpers.getSubUserDir("data") + "/" + Manager.getManager().getProperty("wordlist.file");
-		if ( !new File(wordlist_file).exists() )
-			throw new IOException("The wordlist file does not exist: " + wordlist_file);
+		this.wordlistFile = Helpers.getSubUserDir("data") + "/" + Manager.getManager().getProperty("wordlist.file");
+		this.language = Manager.getInstance().getProperty("wordlist.language");
+
+		if ( !new File(this.wordlistFile).exists() )
+			throw new IOException("The wordlist file does not exist: " + this.wordlistFile);
+
+		List<String> queries = new LinkedList<>();
+		this.createQueries(queries);
 
 		ProxyManager proxyManager = ProxyManager.getInstance();
 		proxyManager.setProxyParser(new HideMyAssHTMLParser());
-		String[] words = FReader.readLines(wordlist_file);
 
-		for ( String word : words ) {
+		for ( String query : queries ) {
 			GoogleWebSearchHTMLParser google = new GoogleWebSearchHTMLParser();
-			proxyManager.fetch(word, google);
-			Meta meta = google.getMeta();
+			proxyManager.fetch(query, google);
 
-			Manager.info(LanguageDetection.class, word, "Estimated Count: " + ((GoogleMeta)meta).getEstimatedCount(), "Count: " + meta.getCount());
+			for ( Result result : google.getResults() ) {
+				this.checkLanguage(language);
+			}
 		}
 	}
-	
+
+	public void createQueries(Collection<String> queries) throws IOException {
+		int averageLength = Manager.getInstance().getIntegerProperty("query.average_lentgh");
+		int maxLength = Manager.getInstance().getIntegerProperty("query.max_length");
+		int maxQueries = Manager.getInstance().getIntegerProperty("query.max_queries");
+
+		Random r = new Random(System.currentTimeMillis());
+
+		String[] words = FReader.readLines(this.wordlistFile);
+		for ( int i = 0; i < maxQueries; i++ ) {
+			String query = "";
+			for ( int j = 0; j < averageLength; j++ )
+				query += r.nextInt(words.length) + " ";
+			query = query.substring(0, query.length() - 1);
+			queries.add(query);
+		}
+	}
+
 	/**
 	 * 
 	 * @param sentence
@@ -87,19 +116,5 @@ public class LanguageDetection {
 			Manager.info("Doc-Language: "+finalLang+"("+finalValue+")");
 		
 		return finalLang.equalsIgnoreCase(this.language);
-	}
-
-	/**
-	 * @return the language
-	 */
-	public String getLanguage() {
-		return language;
-	}
-
-	/**
-	 * @param language the language to set
-	 */
-	public void setLanguage(String language) {
-		this.language = language;
 	}
 }
